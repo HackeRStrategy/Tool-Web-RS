5755151eca04a13158ab74e8d33413ae6f466bc3
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-02-12 18:07:24 -0500

    Add files via upload

---
+++ ToolWeb-RS.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+
+echo -e "\e[1;32m #10%       \e[0m"
+echo -e "\e[1;32m ###20%         \e[0m"
+echo -e "\e[1;32m #####30%        \e[0m"
+echo -e "\e[1;32m #######40%     \e[0m"
+echo -e "\e[1;32m #########50%  \e[0m"
+echo -e "\e[1;32m ###########60% \e[0m"
+echo -e "\e[1;32m #############70%  \e[0m"
+echo -e "\e[1;32m ###############80%  \e[0m"
+echo -e "\e[1;32m #################90%  \e[0m"
+echo -e "\e[1;32m ###################100%   \e[0m"
+echo -e "\e[1;32m #####################110%   \e[0m"
+echo -e "\e[1;32m #######################120%  \e[0m"
+echo -e "\e[1;32m #########################130%   \e[0m"
+echo -e "\e[1;32m ###########################140%   \e[0m"
+echo -e "\e[1;32m #############################150%   \e[0m"
+echo -e "\e[1;32m ###############################160% \e[0m"
+echo -e "\e[1;32m ################################170%   \e[0m"
+echo -e "\e[1;32m ##################################180%   \e[0m"
+echo -e "\e[1;32m ####################################190% \e[0m"
+echo -e "\e[1;32m ######################################200%  \e[0m"
+sleep 4
+echo -e "\e[1;32m #10%       \e[0m"
+echo -e "\e[1;32m ###20%         \e[0m"
+echo -e "\e[1;32m #####30%        \e[0m"
+echo -e "\e[1;32m #######40%     \e[0m"
+echo -e "\e[1;32m #########50%  \e[0m"
+echo -e "\e[1;32m ###########60% \e[0m"
+echo -e "\e[1;32m #############70%  \e[0m"
+echo -e "\e[1;32m ###############80%  \e[0m"
+echo -e "\e[1;32m #################90%  \e[0m"
+echo -e "\e[1;32m ###################100%   \e[0m"
+echo -e "\e[1;32m #####################110%   \e[0m"
+echo -e "\e[1;32m #######################120%  \e[0m"
+echo -e "\e[1;32m #########################130%   \e[0m"
+echo -e "\e[1;32m ###########################140%   \e[0m"
+echo -e "\e[1;32m #############################150%   \e[0m"
+echo -e "\e[1;32m ###############################160% \e[0m"
+echo -e "\e[1;32m ################################170%   \e[0m"
+echo -e "\e[1;32m ##################################180%   \e[0m"
+echo -e "\e[1;32m ####################################190% \e[0m"
+echo -e "\e[1;32m ######################################200%  \e[0m"
+sleep 4
+echo -e "\e[1;32m #10%       \e[0m"
+echo -e "\e[1;32m ###20%         \e[0m"
+echo -e "\e[1;32m #####30%        \e[0m"
+echo -e "\e[1;32m #######40%     \e[0m"
+echo -e "\e[1;32m #########50%  \e[0m"
+echo -e "\e[1;32m ###########60% \e[0m"
+echo -e "\e[1;32m #############70%  \e[0m"
+echo -e "\e[1;32m ###############80%  \e[0m"
+echo -e "\e[1;32m #################90%  \e[0m"
+echo -e "\e[1;32m ###################100%   \e[0m"
+echo -e "\e[1;32m #####################110%   \e[0m"
+echo -e "\e[1;32m #######################120%  \e[0m"
+echo -e "\e[1;32m #########################130%   \e[0m"
+echo -e "\e[1;32m ###########################140%   \e[0m"
+echo -e "\e[1;32m #############################150%   \e[0m"
+echo -e "\e[1;32m ###############################160% \e[0m"
+echo -e "\e[1;32m ################################170%   \e[0m"
+echo -e "\e[1;32m ##################################180%   \e[0m"
+echo -e "\e[1;32m ####################################190% \e[0m"
+echo -e "\e[1;32m ######################################200%  \e[0m"
+sleep 4
+echo -e "\e[1;32m #10%       \e[0m"
+echo -e "\e[1;32m ###20%         \e[0m"
+echo -e "\e[1;32m #####30%        \e[0m"
+echo -e "\e[1;32m #######40%     \e[0m"
+echo -e "\e[1;32m #########50%  \e[0m"
+echo -e "\e[1;32m ###########60% \e[0m"
+echo -e "\e[1;32m #############70%  \e[0m"
+echo -e "\e[1;32m ###############80%  \e[0m"
+echo -e "\e[1;32m #################90%  \e[0m"
+echo -e "\e[1;32m ###################100%   \e[0m"
+echo -e "\e[1;32m #####################110%   \e[0m"
+echo -e "\e[1;32m #######################120%  \e[0m"
+echo -e "\e[1;32m #########################130%   \e[0m"
+echo -e "\e[1;32m ###########################140%   \e[0m"
+echo -e "\e[1;32m #############################150%   \e[0m"
+echo -e "\e[1;32m ###############################160% \e[0m"
+echo -e "\e[1;32m ################################170%   \e[0m"
+echo -e "\e[1;32m ##################################180%   \e[0m"
+echo -e "\e[1;32m ####################################190% \e[0m"
+echo -e "\e[1;32m ######################################200%  \e[0m"
+apt update
+apt upgrade
+pkg install figlet 
+echo
+echo -e "\e[1;32m LEE ATENTAMENTE\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] AHORA LEE ATENTAMENTE LO QUE DEJARE AQUI\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] PARA QUE TODO SE INSTALE CORRECTAMENTE\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] DEBES ACEPTAR A TODO LO QUE SE DESCARGARA EJEMPLO (Y/N) Y \e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] ESCRIBIR Y PARA CONTINUAR, ESO ES TODO...!\e[0m"
+echo
+sleep 15
+echo "========================================="
+echo
+echo
+figlet FIGLET
+echo
+echo "========================================="
+sleep 2
+apt update
+echo "========================================="
+echo
+echo
+figlet UTIL
+figlet LINUX
+echo
+echo "========================================="
+sleep 2
+echo
+echo
+apt install util-linux
+echo
+setterm -foreground green
+echo "========================================="
+echo
+echo
+figlet PHTHON2
+echo 
+echo "========================================="
+sleep 2
+echo
+echo
+pkg install python2
+echo
+setterm -foreground yellow
+echo "========================================="
+echo
+echo
+figlet  NMAP
+echo
+echo "========================================="
+sleep 2
+echo
+echo
+pkg install nmap
+echo
+setterm -foreground green
+echo "========================================="
+echo
+echo
+figlet NETCAT
+echo
+echo "========================================="
+sleep 2
+echo
+echo
+pkg install netcat
+echo
+setterm -foreground yellow
+echo "========================================="
+echo
+echo
+figlet BMON
+echo
+echo "========================================="
+sleep 2
+echo
+echo
+pkg install bmon
+echo
+setterm -foreground green
+echo "========================================="
+echo
+echo
+figlet   GIT
+echo
+echo "========================================="
+sleep 2
+echo
+echo
+pkg install git
+echo
+setterm -foreground yellow
+echo 
+echo "************* INSTALANDO...!***************"
+echo
+git clone https://github.com/HackeRStrategy/Real-Scann-DNS.git 
+echo
+setterm -foreground green 
+echo
+echo "CONGRULATIONS! AHORA TIENE LAS HERRAMIENTAS INSTALADAS "
+echo
+setterm -foreground yellow
+echo " HERRAMIENTAS DESCARGADAS"
+echo
+setterm -foreground blue
+echo
+echo " (1)  NMAP "
+echo " (2)  FIGLET "
+echo " (3)  UTIL-LINUX "
+echo " (4)  REAL-SCANN-DNS "
+echo " (4)  DNS LOOKUP"
+echo " (4)  WHOIS LOKUUP"
+echo " (4)  REVERSE IP LOOKUP"
+echo " (4)  GEO IP LOOKUP"
+echo " (4)  SUBNET LOOKUP"
+echo " (4)  PORT SCANNER"
+echo " (4)  EXTRACT LINKS"
+echo " (4)  ZONE TRASNFER"
+echo " (4)  TTP HEADER"
+echo " (4)  HOST FINDER"
+echo " (5)  PYTHON2"
+echo " (6)  NETCAT"
+echo " (7)  BMON"
+echo
+setterm -foreground cyan
+echo "FACEBOOK : https://facebook.com/RealHackRWAM/ "
+echo
+echo "TELEGRAM : https://t.me/joinchat/AAAAAE010OqRuA5k0hbBiA "
+echo
+echo "YOUTUBE : https://www.youtube.com/channel/UCAIFSeW0e6jTmm8SfWQ8FBg "
+echo
+echo " Tested On : Windows / Linux / Termux"
+echo
+setterm -foreground green
+echo
+echo "              Real Strategy. " 
+echo
+figlet -f digital BY: REAL STRATEGY RS
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[o] PRIMER PASO\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] ESCRIBE LOS COMANDOS 1 & 2\e[0m"
+echo -e "\e[1;32m[1] Escribe: ls\e[0m"
+echo -e "\e[1;32m[2] Escribe: cd Real-Scann-DNS\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[o] SEGUNDO PASO\e[0m"
+echo -e "\e[0]\e[0m"
+echo -e "\e[1;33m[!] ESCRIBE LOS COMANDOS 3 & 4\e[0m"
+echo -e "\e[1;32m[3] Escribe: ls\e[0m"
+echo -e "\e[1;32m[4] Escribe: python2 Real-DNS\e[0m"
+setterm -foreground green 
+figlet -f digital REAL STRATEGY DOMINANDO
+echo -e "\e[1;33m EL CONOCIMIENTO ES LIBRE\e[0m"
+echo
+setterm -foreground green
+echo "***********************************************"
+echo
